3675f468fdf9a8dae205eae7ca625cc20372e133
Leon Piasecki <>
2021-04-16 18:52:44 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("The story of Any cats");
+    Console.WriteLine ("This is a story that I made told in c sharp");
+    Console.WriteLine ("Key: Q: question N: Narrator E: exlamation C: Command S: statement");
+    Console.WriteLine ("E: Let's go outside");
+    Console.WriteLine ("Q: What can we  do");
+    Console.WriteLine ("S: let's go on the trampoline");
+    Console.WriteLine("N: But before they did the cat went on it");
+  }
+}
